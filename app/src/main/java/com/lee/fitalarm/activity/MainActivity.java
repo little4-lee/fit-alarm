f8f3edf955f5c6ddecfdcf5a98dcbff98fe6e67f
little4-lee <watermydream@gmail.com>
2017-11-13 23:57:57 +0800

    first commit

---
+++ app/src/main/java/com/lee/fitalarm/activity/MainActivity.java
@@ -0,0 +1,59 @@
+package com.lee.fitalarm.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.lee.fitalarm.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity_main);
+    }
+
+    @Override
+    protected void onResume () {
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause () {
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop () {
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy () {
+git
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart () {
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart () {
+
+        super.onRestart();
+    }
+
+    @Override
+    protected void onNewIntent (Intent intent) {
+
+        super.onNewIntent(intent);
+    }
+
+}
